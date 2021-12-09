28bcd664eff33575d2cd4a59a69ab09a675233e6
Darek <darekpl9gmail.com>
2021-12-09 13:04:17 +0100

    implemented encrypt/decrypt functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import click
-import os
 
-from RSA_encryption import *
+from pathlib import Path
+from utils import *
 
 
 
@@ -11,28 +11,34 @@ def main():
 
 
 @main.command()
-@click.option("-e", "--encrypt", is_flag=True, help="Encrypts specified file and writes it's content to new file.")
-@click.option("-d", "--decrypt", is_flag=True, help="Decrypts specified file and writes it's content to new file.")
+@click.option("-e", "--encrypt", is_flag=True, help="Encrypts specified file and writes it's content to a new file.")
+@click.option("-d", "--decrypt", is_flag=True, help="Decrypts specified file and writes it's content to a new file.")
 @click.argument("path", type=str)
 def exec(encrypt, decrypt, path):
 
-    if os.path.exists(path):
+    if Path(path).is_file():
+        keys = load_key_pair()
+
+        if not keys:
+            return
+        
+        private_key, public_key = keys
 
         if encrypt:
-            pass
+            encrypt_file(path, public_key)
         elif decrypt:
-            pass
+            decrypt_file(path, private_key)
         else:
-            pass
+            print("\nChoose what to do with file: encrypt/decrypt\n")
 
     else:
-        print("Given file doesn't exists")
+        print("\nGiven file doesn't exists\n")
 
 
 @main.command()
 @click.argument("length", type=int)
 def key(length):
-    pass
+    generate_key_pair(length)
 
 
 if __name__ == "__main__":
